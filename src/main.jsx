d70930b662a364dfac81e19206f2cc20f2dcc427
Theo Millard Tirtawinata <theo.millard@gmail.com>
2023-12-06 22:28:10 +0700

    ASD

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.scss'
-import './style/Form.css'
-import './style/Card.css'
-import './style/Detail.css'
+// import './index.scss'
+// import './style/Form.css'
+// import './style/Card.css'
+// import './style/Detail.css'
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(

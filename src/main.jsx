38ccfda5bd5f0c6fd815940262a3c318c1d3c800
Theo Millard Tirtawinata <theo.millard@gmail.com>
2023-12-06 22:37:57 +0700

    Finishing CSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-// import './style/Form.css'
-// import './style/Card.css'
-// import './style/Detail.css'
+// import './style/Form.css'/
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(

718412bb6ea3fa7195c407124f95a9239ec642c0
Theo Millard Tirtawinata <theo.millard@gmail.com>
2023-12-06 22:29:38 +0700

    ASD

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.scss'
+import './index.scss'
 // import './style/Form.css'
 // import './style/Card.css'
 // import './style/Detail.css'
